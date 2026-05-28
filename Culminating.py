17df52a6ea6bd32cd1e6c7b6a26b434bb410df0f
Devin M <devinmurphy259@gmail.com>
2026-05-28 14:09:45 -0400

    Set up player and player movement

--- Culminating.py
+++ Culminating.py
@@ -16,38 +16,50 @@ BLUE = (0, 0, 255)
 
 # define system constants
 FPS = 60
+#info = pygame.display.Info()
 WIDTH = 640
 HEIGHT = 480
 BGCOLOUR = BLACK ### CHANGE AS NEEDED ###
 
 # initialize pygame, create window, start the clock
 pygame.init()
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
+screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 clock = pygame.time.Clock()
 
 ### ADD YOUR SPRITE CLASSES HERE ###
 
-#class ImageSprite(Sprite):
-#    def __init__(self, x, y, filename):                    # NEW sprite at (x,y)
-#        Sprite.__init__(self)                              # init the Sprite object
-#        self.image = image.load(filename).convert()        # loads the image from filename as the sprite
-#        self.rect = self.image.get_rect()                  # creates the rectangle around the sprite
-#        self.rect.center = (x//2,y//2)
+class ImageSprite(Sprite):
+   def __init__(self, x, y, filename):                    # NEW sprite at (x,y)
+       Sprite.__init__(self)                              # init the Sprite object
+       self.image = image.load(filename).convert()        # loads the image from filename as the sprite
+       self.rect = self.image.get_rect()                  # creates the rectangle around the sprite
+       self.rect.center = (x//2,y//2)
 
-#    # semi-optional part
-#    def update(self):
-#        ### ADD MOVEMENT MODIFIERS HERE ###
-#        self.rect.x += 5
-#        self.rect.y -= 2
-#        self.rect.center = (x//2,y//2)
+   # semi-optional part
+   def update(self):
+       ### ADD MOVEMENT MODIFIERS HERE ###
+       self.rect.x += 5
+       self.rect.y -= 2
+       self.rect.center = (self.rect.x//2,self.rect.y//2
 
+   def setPosition(self, x, y):
+       self.rect.x = x
+       self.rect.y = y
+
+class PlayerSprite(ImageSprite):
+   def moveHorizontal(self, direction):
+        self.rect.x += 5 * direction
+
+   def moveVertical(self, direction):
+        self.rect.y += 5 * direction
 
 ### ADD SPRITE INSTANCES HERE ###
-
-#sprite = ImageSprite(x, y, "INSERT_IMAGE_FILENAME.jpg")
+player = PlayerSprite(0, 0, "mario.png")
+player.image = pygame.transform.scale_by(player.image, 0.3)
+player.setPosition(screen.get_width() / 2, screen.get_height() / 2)
 
 # group sprites
-allSprites = pygame.sprite.Group()
+allSprites = pygame.sprite.Group(player)
 
 # game loop
 running = True
@@ -65,12 +77,16 @@ while running:
     ### ADD ANY GAME LOOP UPDATES HERE ###
 
     # check for keypresses
-    #keys = pygame.key.get_pressed()
+    keys = pygame.key.get_pressed()
 
-    #if keys[K_LEFT]:
-    # do something
-    #if keys[K_RIGHT]:
-    # do something
+    if keys[K_LEFT] or keys[K_a]:
+         player.moveHorizontal(-1)
+    if keys[K_RIGHT] or keys[K_d]:
+        player.moveHorizontal(1)
+    if keys[K_UP] or keys[K_w]:
+        player.moveVertical(-1)
+    if keys[K_DOWN] or keys[K_s]:
+        player.moveVertical(1)
 
     # game loop drawing
     ### ADD ANY GAME LOOP DRAWINGS HERE ###
